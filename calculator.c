7f2e92d73d2ac3224b0e46fcef0a90b80ed50a30
Nishant <crcnish0710@gmail.com>
2022-07-27 11:53:39 +0530

    calc-3

--- calculator.c
+++ calculator.c
@@ -3,30 +3,43 @@ void addition(long int no1,long int no2)
 {
     printf("%ld", no1 + no2);
 }
+
 void subtraction(long int no1,long int no2)
 {
     printf("%ld", no1 - no2);
 }
 
+void multiplication(long int no1,long int no2)
+{
+    printf("%ld", no1 * no2);
+}
+
 int main()
 {
-    printf("Enter number 1 :\n");
-    long int t;
-    scanf("%ld", &t);
-    long int r;
-    printf("Enter number 2 :\n");
-    scanf("%ld", &r);
-    printf("Enter code of the operation needed:\n 1. Add \t 2. Subtract");
-    int n;
-    scanf("%d", &n);
-    switch 👎
+
+    printf("Enter number 1 for further operation :\n");
+    long int no1;
+    scanf("%ld", &no1);
+
+    long int no2;
+    printf("Enter number 2 for further operation :\n");
+    scanf("%ld", &no2);
+
+
+    printf("Enter code of the operation needed:\n 1. Add \t 2. Subtract \t 3. Multiply \n");
+    int check;
+    scanf("%d", &check);
+
+    switch (check)
     {
     case 1:
-        addition(t, r);
+        addition(no1, no2);
         break;
     case 2:
-        subtraction(t, r);
+        subtraction(no1, no2);
         break;
-    
+    case 3:
+        multiplication(no1, no2);
+        break;
     }
 }
