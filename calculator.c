c1e10e690ddcffe60f7a938bacb5291ca3447674
Nishant <crcnish0710@gmail.com>
2022-07-27 11:52:33 +0530

    Calc-2

--- calculator.c
+++ calculator.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
-
-void addition(long int a,long int b)
+void addition(long int no1,long int no2)
 {
-    printf("%ld", a + b);
+    printf("%ld", no1 + no2);
+}
+void subtraction(long int no1,long int no2)
+{
+    printf("%ld", no1 - no2);
 }
 
 int main()
 {
-    printf("Enter num1:\n");
+    printf("Enter number 1 :\n");
     long int t;
     scanf("%ld", &t);
     long int r;
-    printf("Enter num2:\n");
+    printf("Enter number 2 :\n");
     scanf("%ld", &r);
-    addition(t,r);
+    printf("Enter code of the operation needed:\n 1. Add \t 2. Subtract");
+    int n;
+    scanf("%d", &n);
+    switch 👎
+    {
+    case 1:
+        addition(t, r);
+        break;
+    case 2:
+        subtraction(t, r);
+        break;
+    
+    }
 }
